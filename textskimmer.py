56ef1c92f6b80518af60a0f1c0e4a9bbe2c3b533
X1las <dahepix1@gmail.com>
2024-10-01 15:44:27 +0200

    making dedicated files

---
+++ textskimmer.py
@@ -0,0 +1,13 @@
+file = open("output.txt", "r", encoding='utf8')
+
+hardstop = 22
+i = 0
+
+for line in file:
+    split = line.split(' ')
+    print('\n')
+    print(split)
+    if hardstop:
+        i+=1
+    if i == hardstop: 
+        break
