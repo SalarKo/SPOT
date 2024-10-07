d3e621298bdf0fb8163c6fbaab20e1fa45f402c6
X1las <dahepix1@gmail.com>
2024-10-07 21:52:14 +0200

    sandbox-session final commit

--- textskimmer.py
+++ textskimmer.py
@@ -1,5 +1,7 @@
 # Make Skimmer work. RegEx
 import re
+import csv
+
 file = open("output.txt", "r", encoding='utf8')
 content = file.read()
 
@@ -8,7 +10,34 @@ def check_telephone_number(text):
     return re.findall(pattern, text)
 
 def check_duplicates(list):
-
+    templist = []
+    for element in list:
+        try:
+            e = element.replace(" ", "")
+            e = e.replace("-", "")
+            e = e.replace("+", "")
+            number = int(e)
+            ismatch = False
+            for i in list:
+                if number == i:
+                    ismatch = True
+                    break
+            if not ismatch:
+                templist.append(number)
+        except:
+            print("faulty number")
+            continue
+    return templist
 
 numbers = check_telephone_number(content)
-print(numbers)
+refined = check_duplicates(numbers)
+
+formatted_numbers = [[str(number)] for number in refined]
+
+with open("output.csv", 'w', newline='') as csvfile:
+    print("yes")
+    writer = csv.writer(csvfile)
+    writer.writerows(formatted_numbers)
+
+
+print("file made")
